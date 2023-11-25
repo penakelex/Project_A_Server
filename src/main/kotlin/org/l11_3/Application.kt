ba5b4f115c99f6167f964c670740ec1110d5e56f
penakelex <alexeykoninsky129@gmail.com>
2023-11-25 17:25:37 +0700

    Users table addition

--- src/main/kotlin/org/l11_3/Application.kt
+++ src/main/kotlin/org/l11_3/Application.kt
@@ -9,7 +9,6 @@ fun main(args: Array<String>) {
 
 fun Application.module() {
     configureSerialization()
-    configureDatabases()
     configureSecurity()
     configureRouting()
 }
---
+++ src/main/kotlin/org/l11_3/database/tables/Users.kt
@@ -0,0 +1,18 @@
+package org.l11_3.database.tables
+
+import org.jetbrains.exposed.sql.IntegerColumnType
+import org.jetbrains.exposed.sql.Table
+import org.l11_3.database.extensions.array
+
+object Users : Table() {
+    val id = integer("id").autoIncrement()
+    val phone = text("phone")
+    val email = text("email")
+    val password = integer("password")
+    val name = text("name")
+    val surname = text("surname")
+    val patronymic = text("patronymic")
+    val `class` = byte("class")
+    val events = array<Int>("events", IntegerColumnType())
+    val events_participant = array<Int>("events_participant", IntegerColumnType())
+}
